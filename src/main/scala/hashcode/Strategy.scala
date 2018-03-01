00a0714316fdc9deeabaffaaa52fa9f754ef6815
firas16 <firasfbs1989@yahoo.fr>
2018-03-01 20:51:59 +0100

    add utils

--- src/main/scala/hashcode/Strategy.scala
+++ src/main/scala/hashcode/Strategy.scala
@@ -15,12 +15,20 @@ object StrategyFlotte {
   }
 
   // update ccords and time of vehicle if it takes ride
-  def updateVehicleStatus(vehicle: Vehicle, ride: Ride): Vehicle = ???
+  def updateVehicleStatus(vehicle: Vehicle, ride: Ride): Vehicle = {
+    val newT = getRideFinishTime(vehicle, ride)
+    val newPosition = ride.endPosition
+    Vehicle(newPosition, false, newT)
+
+  }
+
+  def get_distance(coord1: Coord, coord2: Coord): Int = Math.abs(coord1.x-coord2.x) + Math.abs(coord1.y-coord2.y)
 
   //returns time when vehicle finishes if it takes ride
   def getRideFinishTime(vehicle: Vehicle, ride: Ride): Int = {
-
-    ???
+    val timeTogetCustomer = vehicle.timeDispo + get_distance(vehicle.coord, ride.startPosition)
+    val timeToTravel = get_distance(ride.startPosition, ride.endPosition)
+    return Math.max(timeTogetCustomer, ride.t1) + timeToTravel
   }
 
 }
