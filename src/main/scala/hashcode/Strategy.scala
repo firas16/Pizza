596706b1c7847abf338c2e0f859ac796952e6789
firas16 <firasfbs1989@yahoo.fr>
2018-03-01 23:44:01 +0100

    final solution with memory problems

--- src/main/scala/hashcode/Strategy.scala
+++ src/main/scala/hashcode/Strategy.scala
@@ -15,24 +15,22 @@ object StrategyFlotte {
 
   }
 
-  def findVehicle(ride: Ride, vehicles: List[Vehicle]): (Option[Int], Option[Vehicle] = {
+  def findVehicle(ride: Ride, vehicles: List[Vehicle]): (Option[Int], Option[Vehicle]) = {
 
-    val validVeh = vehicles.zipWithIndex.filter(v => getRideFinishTime(v._1, ride)<= ride.t2)
-    validVeh match {
-      case Nil => (None,None)
-      case ls =>
-        val rs = ls.sortBy(x => getRideFinishTime(x._1, ride)).head
-        (Some(rs._2), Some(rs._1))
+    val validVeh = vehicles.zipWithIndex.find(v => getRideFinishTime(v._1, ride) <= ride.t2)
+   validVeh match {
+      case None => (None,None)
+      case Some(rs) => (Some(rs._2), Some(rs._1))
     }
 
   }
 
-  def updateFlotte(id: Int, veh: Vehicle, flotte: List[Vehicle]): List[Vehicle] = {
-    val flot = if(id>0) flotte.take(id-1) ++ flotte.drop(id) else flotte.drop(1)
-    flot ++ List(veh)
+  def updateFlote(id: Int, veh: Vehicle, flotte: List[Vehicle]): List[Vehicle] = {
+    if(id>0) flotte.take(id-1) ++ List(veh) ++ flotte.drop(id) else veh :: flotte.drop(1)
+
   }
 
-  def run2(rides: List[Ride], vehicles: List[Vehicle]): List[(Int, Int)] = {
+  def run2(rides: List[Ride], vehicles: List[Vehicle], score: Int): List[(Int, Int)] = {
 
     rides match {
       case ride :: tail =>
@@ -40,9 +38,12 @@ object StrategyFlotte {
         veh match {
           case Some(v) =>
             val updatedVehicle = updateVehicleStatus(v, ride)
-            val updatedFlotte = updatedFlotte(id, updatedVehicle, vehicles)
-            (id.get, ride.index) :: run2(tail, vehicles)
-          case None => run2(tail, vehicles)
+            val updatedFlotte = updateFlote(id.get, updatedVehicle, vehicles)
+            val newScore = get_distance(ride.startPosition, ride.endPosition) + score
+            val bonus = if(v.timeDispo + get_distance(v.coord, ride.startPosition) <= ride.t1) 25 else 0
+            println(newScore + bonus)
+            (id.get, ride.index) :: run2(tail, updatedFlotte, newScore + bonus)
+          case None => run2(tail, vehicles, score)
         }
 
       case Nil => Nil
--- src/main/scala/hashcode/main.scala
+++ src/main/scala/hashcode/main.scala
@@ -8,7 +8,8 @@ object main extends App {
 
   //c_no_hurry
   //d_metropolis
-  val filePath = "src/main/scala/source/c_no_hurry.in"
+  //b_should_be_easy
+  val filePath = "src/main/scala/source/d_metropolis.in"
   for (line <- Source.fromFile(filePath).getLines){
   }
   val lines = Source.fromFile(filePath).getLines.toList
@@ -21,9 +22,14 @@ object main extends App {
   val rides = lines.drop(1).map(line =>  line.split(' ').map(_.toInt)).zipWithIndex
     .map(x => Ride(x._2, Coord(x._1(0), x._1(1)), Coord(x._1(2), x._1(3)), x._1(4), x._1(5)))
 
-  val result = StrategyFlotte.run(grid, rides, vehicles)
-
-  new PrintWriter("resultc.txt") {
+  val sortedRides = rides.sortBy(ride => ride.t1 -> - ride.t2)
+  val result1 = StrategyFlotte.run2( sortedRides, vehicles, 0)
+  println(result1)
+  val result = result1.groupBy(_._1)
+    .map(x => (x._1, x._2.map(_._2)))
+    .toList
+  println(result)
+  new PrintWriter("resultb.txt") {
     result.map(x =>  write(x._2.size.toString +" " + x._2.mkString (" ")  + "\n"))
     close }
 }
